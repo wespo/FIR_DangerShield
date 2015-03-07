2a55debb5388a30d8c5cefaf23e045c0a958307d
wespo <wespo@stanford.edu>
2015-03-06 18:15:46 -0800

    Added Volume Control

--- FIR_DangerShield.ino
+++ FIR_DangerShield.ino
@@ -15,17 +15,21 @@ void setup() {
   DSPShield.startLoopback(); //start shield audio loopback
 
   Serial.begin(9600);
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
+  pinMode(A0, INPUT); //filter center frequency
+  pinMode(A1, INPUT); //filter width
+  pinMode(A2, INPUT); //system volume
 }
 
 void loop() {
     int width = constrain(map(analogRead(A1),20,1000,100,10000),100,10000);
     int center = constrain(map(analogRead(A0),20,1000,20,20000),20,20000);
+    int volume = constrain(map(analogRead(A2),20,1000,0,100),0,100);
     Serial.print("Center Frequency: ");
     Serial.print(center);
     Serial.print("\tWidth: ");
     Serial.print(2*width);
+    Serial.print("\tVolume: ");
+    Serial.print(volume);
     Serial.println();
 
   
@@ -52,5 +56,6 @@ void loop() {
     */
     DSPShield.setFIRFilter(CHAN_BOTH, BAND_PASS, 201, constrain(center-width, 20, 20000), constrain(center+width,20,20000));
     //try 511 taps for a sharper notch but slower load times!
+    DSPShield.setOutputVolume(volume);
 }
 
